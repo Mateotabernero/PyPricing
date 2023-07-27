90f1a4fec400773948ea417bfe0cbd6f6974e8d3
Mateotabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-07-27 16:00:19 +0200

    Update BinOP.py

--- BinOP.py
+++ BinOP.py
@@ -25,17 +25,18 @@ def binOP (r, sigma, S, T, K, put_or_call, optionType, M):
     delta_t = T / M 
 
     # Creation of the tree of stock prices and the tree of option value (We don't really need to create the price tree, we just need the last one. the second s array is that case) (For american ption is necessary)
+    # No ahorraríamos mucho si metemos lo de S dentro de los if, porque ya estamos haciendo O(M^2) work anyways 
     S = [[0 for _ in range (i+1)] for i in range (M+1)] 
     V = [[0 for _ in range (i+1)] for i in range (M+1)]
     V_cont = [[0 for _ in range(i+1)] for i in range(M+1)]
-    S_2 =[0 for _ in range(M+1)]
+    
     
     # Boundary values
 
     S[0,0] = S 
     for j in range (M+1): 
         S[j, M] = S[0,0] * (u**j) * (d** (M-j)) 
-        S_2 [j] = S *(u**j)*(d**(M-j))
+        
     
     for j in range (M+1):
         V[j,M]   = payOff(S[j,M], K, put_or_call)
@@ -50,8 +51,9 @@ def binOP (r, sigma, S, T, K, put_or_call, optionType, M):
     elif optionType == 'A':
         for i in reversed(range(M)):
             for j in range(i):
+                S[j,i] = S[0,0] * (u**j) * (d** (i-j))
                 V_cont[j,i] = math.exp(-r*delta_t) *(p*V[j+1, i+1] + (1-p)*V[j,i+1])
-                V[j,i]      = payOff (S,K,put_or_call, bound = V_cont[j,i])        
+                V[j,i]      = payOff (S[j,i],K,put_or_call, bound = V_cont[j,i])        
 
     else:
         raise ValueError ("optionType has to be European ('E') or American ('A')")
