2771a966492711067b2a6d85cf5e928ee1e094c2
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-12 12:00:08 +0200

    Update helpFunctions.py

--- helpFunctions.py
+++ helpFunctions.py
@@ -1,18 +1,5 @@
 ### THIS FILE INCLUDES SEVERAL FUNCTIONS THAT WILL BE HELPFUL IN THE OTHER ONES ###
 
-
-
-
-
-# This function calculates the log returns from a series of prices and then compute the mean (as an approximation of mu) and the std (as an approximation of sigma)
-def mu_sigma(prices):
-  log_returns = np.zeros(len(log_prices)-1)
-  for i in range(len(log_returns)):
-    log_returns[i] = math.log(prices[i+1]/prices[i])
-  mu = np.mean(log_returns)
-  sigma = np.std(returns) 
-  return (mu, sigma) 
-
 # A function to calculate the (vanilla) payOff of a function
 def payOff (S,K, put_or_call): 
     if (put_or_call == 'C'): 
@@ -34,3 +21,12 @@ def asianPayOff(S,K, mean, put_or_call):
   else: 
     raise ValueError ("Please choose a valid value for put_or_call: Price Call ('PC')', Price Put ('PP'), Strike Call ('SC') or Strike Put ('SP')")
 
+
+# This function calculates the log returns from a series of prices and then compute the mean (as an approximation of mu) and the std (as an approximation of sigma)
+def mu_sigma(prices):
+  log_returns = np.zeros(len(log_prices)-1)
+  for i in range(len(log_returns)):
+    log_returns[i] = math.log(prices[i+1]/prices[i])
+  mu = np.mean(log_returns)
+  sigma = np.std(returns) 
+  return (mu, sigma) 
