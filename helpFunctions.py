68ef255a7e2f77dad7de596158095788212faa2f
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-12 11:02:03 +0200

    Update helpFunctions.py

--- helpFunctions.py
+++ helpFunctions.py
@@ -13,3 +13,11 @@ def mu_sigma(prices):
   sigma = np.std(returns) 
   return (mu, sigma) 
 
+# A function to calculate the (vanilla) payOff of a function
+def payOff (S,K, put_or_call): 
+    if (put_or_call == 'C'): 
+        return max(S-K,0) 
+    elif (put_or_call == 'P'):
+        return max (K-S, 0) 
+
+
