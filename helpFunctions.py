a23166c06426de7430714a3963dfb879e7af49e5
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-12 17:27:35 +0200

    Update helpFunctions.py

--- helpFunctions.py
+++ helpFunctions.py
@@ -1,15 +1,32 @@
 ### THIS FILE INCLUDES SEVERAL FUNCTIONS THAT WILL BE HELPFUL IN THE OTHER ONES ###
 
 # A function to calculate the (vanilla) payOff of a function
-def payOff (S,K, put_or_call): 
+def payOff (S,K, put_or_call):
+    """
+    Calculate the payoff of an American or European option at some moment
+
+    : param S            : Current price of the underlying stock
+    : param K            : Exercise price 
+    : param put_or_call  : Is the option a put ('P') or a call ('C')?
+    : return             : payoff of the option 
+    """
     if (put_or_call == 'C'): 
         return max(S-K,0) 
     elif (put_or_call == 'P'):
         return max (K-S, 0) 
     else: 
         raise ValueError ("Please choose a valid value for put_or_call: Put ('P') or Call ('C')") 
-# Hay muchas formas de implementar esto realmente, podría en la función principal calcular la media y según put_or_call poner K = S o S = mean y luego usar payOff. Pero bueno, está bien 
+        
 def asianPayOff(S,K, mean, put_or_call):
+    """
+    Calculate the payoff of an Asian option 
+    
+    : param S           : Current price of the underlying stock 
+    : param K           : Exercise price 
+    : mean              : Mean of the stock prices during the lifetime of the option 
+    : put_or_call       : Type of Asian option: price call ('PC'), price put ('PP'), strike call ('SC') or strike put ('SP')
+    : return            : payoff of the option
+    """
   if (put_or_call == 'PC'):
     return max(mean-K, 0) 
   if (put_or_call == 'PP'):
@@ -21,9 +38,12 @@ def asianPayOff(S,K, mean, put_or_call):
   else: 
     raise ValueError ("Please choose a valid value for put_or_call: Price Call ('PC')', Price Put ('PP'), Strike Call ('SC') or Strike Put ('SP')")
 
-
-# This function calculates the log returns from a series of prices and then compute the mean (as an approximation of mu) and the std (as an approximation of sigma)
 def mu_sigma(prices):
+    """
+    Calculate the log returns from a series of prices and then compute the mean (as an approximation of the drift) and the std (as an approximation of the volatility) 
+
+    : param prices       : An array with the prices of 
+    """
   log_returns = np.zeros(len(log_prices)-1)
   for i in range(len(log_returns)):
     log_returns[i] = math.log(prices[i+1]/prices[i])
