f937988deab73995e6b82a2c977baf1a97f62f9e
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-09-06 18:49:52 +0200

    Update payOffs.py

--- MC/payOffs.py
+++ MC/payOffs.py
@@ -1,4 +1,13 @@
-def EuPayOff(S, K, call_or_put): 
+def EuPayOff(S, K, call_or_put):
+    """
+    Calculate the payoff of random paths of an European option
+
+    : param S           : Numpy array of the paths 
+    : param K           : Strike price
+    : param call_or_put : Whether the option is a call ('C') or a put ('P') 
+    : return            : numpy array containing the payoff of each path 
+    """
+    
     if call_or_put == 'C':
         return np.maximum(S[:,-1] - K, 0) 
     elif call_or_put =='P': 
@@ -7,6 +16,16 @@ def EuPayOff(S, K, call_or_put):
         raise ValueError("Please choose an appropiate value for call_or_put (Call ('C') or Put ('P')") 
 
 def AsPayOff(S, K, call_or_put):
+    """
+    Calculate the payoff of random paths of an American option
+
+    : param S           : Numpy array of the paths 
+    : param K           : Strike price
+    : param call_or_put : Whether the option is a price call ('PC'), a price put ('PP'), a strike call ('SC') or a strike put ('SP')  
+    : return            : numpy array containing the payoff of each path 
+    """
+    
+    
     A = np.zeros(S.shape[0]) 
     for i in range(len(A)): 
         A[i] = np.mean(S[i,:]) - S[i,0]/2 - S[i,-1]/2
