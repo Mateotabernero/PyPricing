c97ec792124f875ba77b2be45c88751ae6b3892e
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-09-06 18:29:08 +0200

    Update main.py

--- MC/main.py
+++ MC/main.py
@@ -1,8 +1,13 @@
 import GBM 
 import heston 
 import payOffs
+import numpy as np 
+
+EuPayOff = payOffs.EuPayOff 
+AsPayOff = payOffs.AsPayOff 
+
 class MCGBMOption: 
-    def __init__(self, spot_price, strike_price, maturity, call_or_put, risk_free_rate, volatility, payOff = EuPayOff, *args): 
+    def __init__(self, risk_free_rate, volatility, spot_price, strike_price, maturity, call_or_put, payOff = EuPayOff): 
         self.S   = spot_price 
         self.K   = strike_price 
         self.T   = maturity 
@@ -11,7 +16,6 @@ class MCGBMOption:
         
         self.payOff     = payOff
         self.CoP        = call_or_put
-
     # Aquí definiría las payOffs según el tipo de opción con la que estemos tratando y la definiría como un atributo de clase
 
 
@@ -23,39 +27,37 @@ class MCGBMOption:
 
     def price(self, num_steps, num_simulations = 10**4, integration_method = 'E', ant_variates = False): 
 
-        # Aquí creo que da un poco igual si cogemos self.generate_paths o la función original GBM.GBM. Si al ginal son iguales 
-        # Esta igual mejor porque no hay que meter tantos argumentos (como r, sigma, o T) 
+
 
         S =  self.generate_paths(num_steps, num_simulations = num_simulations, integration_method = integration_method, ant_variates = ant_variates)
         
-        return np.exp(-self.r*self.T)*(np.mean(self.payOff(S, self.K, call_or_put, *args))) 
+        return np.exp(-self.r*self.T)*(np.mean(self.payOff(S, self.K, self.CoP))) 
 
     def delta(self, num_steps, num_simulations = 10**4, integration_method = 'E', ant_variates = False):
-        new_S = self.S*1.01
+        new_S0 = self.S*1.01
 
         price = self.price(num_steps, num_simulations, integration_method, ant_variates) 
 
-        new_S = GBM.GBM(self.r, self.sig, new_S, num_steps, self.T, num_simulations, integration_method, ant_variates)
-        new_price = np.exp(-self.r*self.T)*(np.mean(self.payOff(new_S, self.K, call_or_put, *args)))
+        new_S = GBM.GBM(self.r, self.sig, new_S0, num_steps, self.T, num_simulations, integration_method, ant_variates)
+        new_price = np.exp(-self.r*self.T)*(np.mean(self.payOff(new_S, self.K, self.CoP)))
          
-        return (new_price - price)/(new_S - self.S) 
+        return (new_price - price)/(new_S0 - self.S) 
 
     def vega(self, num_steps, num_simulations = 10**4, integration_method = 'E', ant_variates = False): 
         new_sig = self.sig*1.01
-
         price = self.price(num_steps, num_simulations, integration_method, ant_variates) 
 
         new_S = GBM.GBM(self.r, new_sig, self.S, num_steps, self.T, num_simulations, integration_method, ant_variates)
-        new_price = np.exp(-self.r*self.T)*(np.mean(self.payOff(new_S, self.K, call_or_put, *args)))
+        new_price = np.exp(-self.r*self.T)*(np.mean(self.payOff(new_S, self.K, self.CoP)))
 
-        return (new_price - price)/(new_S - self.S) 
+        return -(new_price - price)/(new_sig- self.sig) 
     
     def theta(self, num_steps, num_simulations = 10**4, integration_method = 'E', ant_variates = False): 
         # Still need to define the function 
         pass 
         
 class MCHestonOption: 
-    def __init__(self, spot_price, strike_price, maturity, risk_free_rate, volatility_0, kappa, theta, xi, payOff = EuPayOff): 
+    def __init__(self,  risk_free_rate, volatility_0, spot_price, strike_price, maturity, kappa, theta, xi,call_or_put, payOff = EuPayOff): 
         self.S     = spot_price
         self.K     = strike_price 
         self.T     = maturity 
@@ -64,6 +66,8 @@ class MCHestonOption:
         self.kappa = kappa 
         self.theta = theta 
         self.xi    = xi 
+        self.payOff = payOff
+        self.CoP   = call_or_put
 
     def generate_paths(self, num_steps, num_simulations = 10**4, corr_index = 0): 
         S, v = heston.heston(self.r, self.v_0, self.S, self.theta, self.xi, num_steps, corr_index = corr_index, num_simulations = num_simulations) 
@@ -73,20 +77,18 @@ class MCHestonOption:
 
         S, v = self.generate_paths(num_steps, num_simulations = num_simulations, corr_index= corr_index)
         
-        return np.exp(-self.r*self.T)*(np.mean(self.payOff(S, self.K, call_or_put, *args)))
+        return np.exp(-self.r*self.T)*(np.mean(self.payOff(S, self.K, self.CoP)))
     
     def delta(self, num_steps, num_simulations = 10**4, corr_index = 0): 
-        new_S = self.S*1.01
+        new_S0 = self.S*1.01
         
         price = self.price(num_steps, num_simulations, corr_index) 
 
-        new_S, new_v = heston.heston(self.r, self.v_0, new_S, self.theta, self.xi, num_steps, corr_index, num_simulations) 
+        new_S, new_v = heston.heston(self.r, self.v_0, new_S0, self.theta, self.xi, num_steps, corr_index, num_simulations) 
         
-        new_price = np.exp(-self.r*self.T)*np.mean(self.payOff(new_S, self.K, call_or_put, *args)) 
+        new_price = np.exp(-self.r*self.T)*np.mean(self.payOff(new_S, self.K, self.CoP)) 
 
-        return (new_price - price)/(new_S - self.S) 
+        return (new_price - price)/(new_S0 - self.S) 
     
 
 
-EuPayOff = payOffs.EuPayOff 
-AmPayOff = payOffs.AmPayOff 
