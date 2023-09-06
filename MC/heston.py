bf0a579f0ea99b10e4e6550535ee0c6ec726221f
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-09-06 16:31:15 +0200

    Update heston.py

--- MC/heston.py
+++ MC/heston.py
@@ -1,6 +1,23 @@
 import numpy as np
 
 def heston(r, sigma_0, S_0, kappa, theta, xi, num_steps, T, corr_index = 0, num_simulations = 10000): 
+    """
+    Calculate sample paths of Heston model 
+
+    : param r                  : The drift 
+    : param sigma_0            : The initial volatility 
+    : param S_0                : The initial price 
+    : param kappa              : Kappa parameter in Heston model 
+    : param theta              : Theta parameter in Heston model 
+    : param xi                 ; Xi parameter in Heston model 
+    : param num_steps          : Number of steps for the random path generation  
+    : param T                  : Time length of the generated path 
+    : param corr_index         : Correlation index between the interest rate Wiener process and the volatility Wiener process
+    : param num_simulations    : The number of paths generated (default value: 10000)  
+    : returns                  : A numpy array containing the generated paths of both the prices and the volatility     
+    
+    """
+
     delta_t = T/num_steps
 
     S = np.zeros((num_simulations, num_steps +1))
