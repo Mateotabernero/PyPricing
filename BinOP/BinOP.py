0e405488e1b7794e5fc374fc659f556be59157b5
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-09-07 17:18:48 +0200

    Update BinOP.py

--- BinOP/BinOP.py
+++ BinOP/BinOP.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 ### We first include some functions to compute some useful magnitudes on Binomial Option pricing (all to be explained in the Readme file) 
 def payOff (S,K, put_or_call):
@@ -26,7 +26,7 @@ def calc_beta (r, sigma, delta_t, gamma_par = 1):
     : oaram gamma_par    : The gamma parameter in BOP (default value: 1) 
     : return             : beta 
     """
-    return 1/2 *(gamma_par *math.exp(-r*delta_t) + math.exp((r+sigma**2)*delta_t))
+    return 1/2 *(gamma_par *np.exp(-r*delta_t) + np.exp((r+sigma**2)*delta_t))
 
 def calc_u(beta, gamma_par = 1): 
     """
@@ -35,7 +35,7 @@ def calc_u(beta, gamma_par = 1):
     : param gamma_par    : The gamma parameter in BOP (default value: 1) 
     : return             : u, the factor by which S moves up in Binomial Option Pricing
     """
-    return (beta + math.sqrt(beta**2 - gamma_par)) 
+    return (beta + np.sqrt(beta**2 - gamma_par)) 
     
 def calc_d(beta, gamma_par= 1):
     """
@@ -44,7 +44,7 @@ def calc_d(beta, gamma_par= 1):
     : param gamma_par    : The gamma parameter in BOP (default value: 1) 
     : return             : d, the factor by which S moves down in BOP 
     """
-    return (beta - math.sqrt(beta**2 - gamma_par))
+    return (beta - np.sqrt(beta**2 - gamma_par))
 
 def calc_p(r, delta_t, u, d):
     """
@@ -55,12 +55,12 @@ def calc_p(r, delta_t, u, d):
     : param d            : d, the factor by which S moves down in BOP
     : return             : p, the probability of S moving up 
     """
-    return (math.exp (r*delta_t)-d) / (u-d) 
+    return (np.exp (r*delta_t)-d) / (u-d) 
 
 
 # To improve convergence gamma = e^((2/M)*log(K/S_0)) is a good choice
 
-def generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = 1): 
+def generate_tree(r, sigma, S_0, K, T, call_or_put, optionType, M, gamma_par = 1): 
     """
     Calculate the Binomial Tree for American or European option 
     : param r            : Risk-free interest rate 
@@ -68,7 +68,7 @@ def generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = 1
     : param S_0          : Spot price of the underlying stock 
     : param T            : Expiration time 
     : K                  : Strike price
-    : param put_or_call  : Put ('P') or Call ('C') 
+    : param call_or_pit  : Put ('P') or Call ('C') 
     : param optionType   : Is it an European ('E') or American ('A') option? 
     : param M            : Number of steps in the binomial tree 
     : param gamma_par    : Gamma parameter in binomial option pricing (default value: 1) 
@@ -82,7 +82,6 @@ def generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = 1
     d = calc_d (beta, gamma_par = gamma_par) 
     p = calc_p (r, delta_t, u, d)
 
-    # Creation of the tree of stock prices and the tree of option value (We don't really need to create the price tree, we just need the last one. the second s array is that case) (For american ption is necessary)
 
     S= [[0 for _ in range (i+1)] for i in range (M+1)] 
     V = [[0 for _ in range (i+1)] for i in range (M+1)]
@@ -98,13 +97,13 @@ def generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = 1
     
     for j in range (M+1):
         
-        V[M][j]   = payOff(S[M][j], K, put_or_call)
+        V[M][j]   = payOff(S[M][j], K, call_or_put)
 
     if optionType == 'E':
         for i in reversed(range (M)):
             for j in range(i+1):
                 S[i][j] = S[0][0] * (u**j) * (d** (i-j))
-                V[i][j] = math.exp(-r*delta_t)*(p*V[i+1][j+1] + (1-p)*V[i+1][j])
+                V[i][j] = np.exp(-r*delta_t)*(p*V[i+1][j+1] + (1-p)*V[i+1][j])
 
     
     elif optionType == 'A':
@@ -112,13 +111,13 @@ def generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = 1
             for j in range(i+1):
                 S[i][j] = S[0][0] * (u**j) * (d** (i-j))
                 
-                V_cont[i][j] = math.exp(-r*delta_t) *(p*(V[i+1][j+1]) + (1-p)*V[i+1][j])
-                V[i][j]      = max(payOff (S[i][j],K,put_or_call), V_cont[i][j])       
+                V_cont[i][j] = np.exp(-r*delta_t) *(p*(V[i+1][j+1]) + (1-p)*V[i+1][j])
+                V[i][j]      = max(payOff (S[i][j],K, call_or_put), V_cont[i][j])       
 
     else:
         raise ValueError ("optionType has to be European ('E') or American ('A')")
-    return V 
+    return V, S 
 
-def valueBinOp(r, sigma, S_0, K, T put_or_call, optionType, M, gamma_par = 1): 
-    V, S = generate_tree(r, sigma, S_0, T, K, put_or_call, optionType, M, gamma_par = gamma_par)
+def valueBinOp(r, sigma, S_0, K, T, call_or_put, optionType, M, gamma_par = 1): 
+    V, S = generate_tree(r, sigma, S_0, K, T,  call_or_put, optionType, M, gamma_par = gamma_par)
     return V[0][0]
